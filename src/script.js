36925b41ab97dbdcb2166419cd37687de1d5b77e
Ameer <ameerapex496@gmail.com>
2023-11-19 13:39:44 +0100

    build the project - merged header

--- src/script.js
+++ src/script.js
@@ -4,13 +4,12 @@ var searchBtn = document.querySelector(".search-Icon");
 // our global varibales
 // our functions
 var openHandler = function () {
-    console.log("hallo world");
-    if (!textBox.classList.contains("active")) {
-        textBox.classList.add("active");
-    }
-    else if (textBox.classList.contains("active")) {
-        textBox.classList.remove("active");
-    }
+  console.log("hallo world");
+  if (!textBox.classList.contains("active")) {
+    textBox.classList.add("active");
+  } else if (textBox.classList.contains("active")) {
+    textBox.classList.remove("active");
+  }
 };
 // our event lisnters
 searchBtn.addEventListener("click", openHandler);
